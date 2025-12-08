14b41bb554e90610ff057bb557080dfa9594bbd5
eckennedy-del <eckennedy@college.harvard.edu>
2025-12-07 21:44:26 -0500

    background color change (cool-toned)

--- CS50 IOS/Views/ContentView.swift
+++ CS50 IOS/Views/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
     var body: some View {
         TabView {
             // MAP TAB
-            MapViewModel.MapKitContentView()
+            MapKitContentView()
                 .tabItem {
                     Label("Map", systemImage: "map")
                 }
